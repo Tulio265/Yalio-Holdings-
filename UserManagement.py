b89cbb8650f75f187d00ee57a18991b686b7caee
Macdonald Juma <macdonaldjumah95@gmail.com>
2023-02-02 08:27:37 +0200

    Changed Background Colours and added yalio Holding Ltd Logo

--- UserManagement.py
+++ UserManagement.py
@@ -6,7 +6,7 @@ import ast
 root = Tk()
 root.title('Yalio Holdings - Login')
 root.resizable(False,False)
-root.configure(bg='#91a3b0')
+root.configure(bg='#343131')
 
 #In the code below , winfo_screenwidth and winfo_screenheight methods are used to get the screen dimensions,
 # and the window dimensions are set using geometry method. The x and y coordinates of the window are calculated
@@ -22,13 +22,13 @@ root.geometry("%dx%d+%d+%d" % (window_width, window_height, x_coordinate, y_coor
 
 
 login_icon = PhotoImage(file='C:\\Users\\Aramex\\OneDrive\\Pictures\\Saved Pictures\\login.png')
-Label(root,image=login_icon,border='0',bg='#91a3b0',).place(y=100,x=50)
+Label(root,image=login_icon,border='0',bg='#343131',).place(y=50,x=50)
 
-login_frame = Frame(root,width=350,height=390,bg='#91a3b0')
+login_frame = Frame(root,width=350,height=390,bg='#343131')
 #login_frame.config(borderwidth=5,relief='ridge')
 login_frame.place(y=50,x=480)
 
-login_label = ttk.Label(login_frame,foreground='#9bddff',background='#91a3b0',text='Login',font=('Dubai',22,'bold'))
+login_label = ttk.Label(login_frame,foreground='#9bddff',background='#343131',text='Login',font=('Dubai',22,'bold'))
 #login_label.grid(column=3,row=1)
 login_label.place(y=50,x=5)
 
@@ -38,7 +38,7 @@ def on_leave(e):
     if username_entry.get()=='':
         username_entry.insert(0,'Usename or Email')
 
-username_entry =Entry(login_frame,width=25,border=0,background='#91a3b0',foreground='#9bddff',font=('Dubai',11))
+username_entry =Entry(login_frame,width=25,border=0,background='#343131',foreground='#9bddff',font=('Dubai',11))
 username_entry.place(y=100,x=10)
 username_entry.insert(0,'Username or Email')
 Frame(login_frame,width=256,height=2,bg='white').place(y=122,x=10)
@@ -62,7 +62,7 @@ password_entry = Entry(
     login_frame,
     width=25,
     border=0,
-    background="#91a3b0",
+    background="#343131",
     foreground="#9bddff",
     font=("Dubai", 11),
 )
@@ -81,6 +81,6 @@ password_entry.bind("<FocusOut>", on_leave)
 password_entry.bind("<Key>", on_entry)
 
 
-Button(login_frame,width=30,pady=7,text='Login',bg='#9bddff',fg='white').place(x=30,y=200)
+Button(login_frame,width=30,pady=7,text='Login',bg='#343131',fg='white').place(x=30,y=200)
 
 root.mainloop()
